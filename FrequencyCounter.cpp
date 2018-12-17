cd200e70089f48f6bb7b210e5064b3ba02273766
Elzawawy <amr22.elzawawy@ymail.com>
2018-12-17 19:37:02 +0200

    Frequency Counter Module added. Not implemenetd.

---
+++ FrequencyCounter.cpp
@@ -0,0 +1,9 @@
+using namespace std;
+#include <string.h>
+#include <iostream>
+#include "FrequencyCounter.h"
+
+void readFile(string fileName)
+{
+
+}
---
+++ FrequencyCounter.h
@@ -0,0 +1,11 @@
+/*
+*
+* FrequencyCounter module is to read input file and count frequency for each character.
+*
+*/
+#ifndef FREQUENCY_COUNTER_H_
+#define FREQUENCY_COUNTER_H_
+
+void readFile(string fileName);
+
+#endif  /*FREQUENCY_COUNTER_H_*/
