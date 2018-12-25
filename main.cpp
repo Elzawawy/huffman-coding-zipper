7d9c6731977d1bf6cbf9141584fdf875ecdc974d
Elzawawy <amr22.elzawawy@ymail.com>
2018-12-25 04:25:35 +0200

    Derived Output methods in main to show the algorithm performance in time and space.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "FrequencyCounter.h"
 #include "Huffman.h"
 
+std::ifstream::pos_type filesize(const char* filename);
+
 int main()
 {
     FrequencyCounter frequencyCounter ;
@@ -9,12 +11,29 @@ int main()
     cin >> workingMode;
     if(workingMode == "compress")
     {
+        clock_t tStart = clock();
         frequencyCounter.readFile("../input.txt");
         huffman.huffer(frequencyCounter.getFrequencyMap());
         huffman.compressTofile("../input.txt","../output.txt");
+        cout <<"Time taken: "<<(1.0*(clock() - tStart)/CLOCKS_PER_SEC)<<"sec"<<endl;
+        cout << "Input File Size : "<<filesize("../input.txt")<<" bytes."<<endl;
+        cout<< "Compressed File Size : "<<filesize("../output.txt")<<" bytes."<<endl;
     }
     else if(workingMode == "decompress")
+    {
+        clock_t tStart = clock();
         huffman.deHuffer("../output.txt","../output2.txt");
+        cout <<"Time taken: "<<(1.0*(clock() - tStart)/CLOCKS_PER_SEC)<<"sec"<<endl;
+        cout << "Input File (Compressed) Size : "<<filesize("../output.txt")<<" bytes."<<endl;
+        cout<< "DeCompressed File Size : "<<filesize("../output2.txt")<<" bytes."<<endl;
+    }
+
 
     return 0;
+}
+
+std::ifstream::pos_type filesize(const char* filename)
+{
+    std::ifstream in(filename, std::ifstream::ate | std::ifstream::binary);
+    return in.tellg();
 }
